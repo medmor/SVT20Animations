8eb97a9687c19405e0313f7f7538d5a4445c6592
medmor <enakreb@hotmail.com>
2024-04-24 23:04:26 +0100

    com

---
+++ Assets/Scripts/Monobehaviours/DemographiqueEvolution.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemographiqueEvolution : MonoBehaviour
+{
+    int[] population = new int { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    int[] years = new int { 1960, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
