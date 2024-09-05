f4e355eb379cbf018a9ddaa62abf0fee22bdf826
medmor <enakreb@hotmail.com>
2024-09-05 18:28:36 +0100

    com

--- Assets/Scripts/Monobehaviours/TimelineManager.cs
+++ Assets/Scripts/Monobehaviours/TimelineManager.cs
@@ -1,13 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Playables;
 
 public class TimelineManager : MonoBehaviour
 {
-    [SerializeField] Playable[] playables;
+    [SerializeField] GameObject[] objects;
     void Start()
     {
-        playables[0].Play();
-        var duration = playables[0].GetDuration();
+        foreach (var obj in objects)
+        {
+            obj.SetActive(false);
+        }
+        StartCoroutine(PlayAll());
+    }
+
+    IEnumerator PlayAll()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i].SetActive(true);
+            var play = objects[i].GetComponentInChildren<PlayableDirector>();
+            play.Play();
+            yield return new WaitForSeconds((float)play.duration);
+            objects[i].SetActive(false);
+        }
     }
 
 
